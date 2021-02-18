e1c5718e617c6dd2be8d0fc5aea66d2037395a4f
KolesnikIgor <kiv7077@gmail.com>
2021-02-18 23:18:48 +0200

    Added Navigation and Table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import {Component} from 'react';
+import teamIcon from "./images/team.svg";
+import toggleIcon from "./images/toggle.svg";
+
 
 function Navigation({date, next, prev}) {
   return <div className="calendarBar">
@@ -9,15 +12,54 @@ function Navigation({date, next, prev}) {
   </div>
 }
 
-function Table({allDays}) {
+function Table({allDays, teams, date}) {
+  let Rows = [];
+
+  if (teams && allDays) {
+    teams.forEach((team, i) => {
+      Rows.push(
+        <>
+          <tr className="calendarTable__team-header">
+            <td key={i}>
+              <div className="calendarTable__team-title">
+                <span className="calendarTable__team-name">{team.name}</span>>
+                <span>
+                  <img src={teamIcon}/>
+                  <span className="calendarTable__team-count">{team.members.length}</span>
+                </span>
+                <span className="calendarTable__percentage">{team.percentageOfAbsent[date.getMonth()]}%</span>
+                <button className="calendarTable__team-toogle">
+                  <img src={toggleIcon}/>
+                </button>
+              </div>
+            </td>
+            {allDays.map((day, i) => {
+                return <td key={i} className={day.isDayOff ? "calendarTable__dayOff" : ""}>
+                </td>
+              }
+            )}
+          </tr>
+          {team.members.map(((member, i) => {
+            return <tr>
+              <td key={i}>{member.name}</td>
+              {allDays.map((day, i) => {
+                  return <td key={i} className={day.isDayOff ? "calendarTable__dayOff" : ""}>
+                  </td>
+                }
+              )}
+            </tr>
+          }))}
+        </>)
+    })
+  }
   return <table className="calendarTable">
     <thead>
     <tr>
       <td className="calendarTable__addVocation">
         <button>Add Vacation</button>
       </td>
-      {allDays.map(day => {
-          return <td className={day.isDayOff ? "calendarTable__dayOff" : ""}>
+      {allDays.map((day, i) => {
+          return <td key={i} className={day.isDayOff ? "calendarTable__dayOff" : ""}>
             <span>{day.dayName}</span>
             <span>{day.date}</span>
           </td>
@@ -25,6 +67,12 @@ function Table({allDays}) {
       )}
     </tr>
     </thead>
+    {teams
+      ? <tbody>{Rows}</tbody>
+      : <tr className="loading__wrapper">
+        <div className="lds-dual-ring"></div>
+      </tr>
+    }
   </table>
 }
 
@@ -38,7 +86,7 @@ class App extends Component {
     }
     this.nextMonth = this.nextMonth.bind(this);
     this.prevMonth = this.prevMonth.bind(this);
-    // this.fetchCalendar = this.fetchCalendar.bind(this);
+    this.fetchCalendar = this.fetchCalendar.bind(this);
   }
 
   nextMonth() {
@@ -66,14 +114,78 @@ class App extends Component {
       });
       date.setDate(date.getDate() + 1);
     }
-    this.setState({allMonthDays: days});
+    this.setState({allMonthDays: days})
   }
 
+  fetchCalendar() {
+    const departmentTeams = {
+      teams: [
+        {
+          name: "Frontend Team",
+          percentageOfAbsent: [0, 2, 0, 0, 1, 22, 2, 2, 2, 2, 11, 1],
+          members: [
+            {
+              name: "FE_Team_User1",
+              vacations: [
+                {startDate: "20.02.2021", endDate: "22.03.2021", type: "Paid"},
+                {startDate: "20.11.2020", endDate: "22.11.2020", type: "Paid"},
+              ],
+            },
+            {
+              name: "FE_Team_User1",
+              vacations: [
+                {startDate: "20.02.2020", endDate: "22.02.2020", type: "UnPaid"},
+                {startDate: "20.03.2020", endDate: "22.03.2020", type: "UnPaid"},
+              ],
+            },
+          ],
+        },
+        {
+          name: "Backend Team",
+          percentageOfAbsent: [0, 2, 0, 0, 1, 2, 2, 2, 2, 2, 1, 1],
+          members: [
+            {
+              name: "FE_Team_User1",
+              vacations: [
+                {startDate: "15.02.2020", endDate: "22.02.2020", type: "UnPaid"},
+                {startDate: "20.03.2020", endDate: "22.03.2020", type: "UnPaid"},
+              ],
+            },
+            {
+              name: "FE_Team_User1",
+              vacations: [
+                {startDate: "20.02.2020", endDate: "22.02.2020", type: "UnPaid"},
+                {startDate: "20.03.2020", endDate: "22.03.2020", type: "UnPaid"},
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    return fetch("https://jsonplaceholder.typicode.com/posts/1", {
+      method: "PUT",
+      body: JSON.stringify(departmentTeams),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({teams: json.teams});
+      });
+  }
+
+  componentDidMount() {
+    this.getDaysOfActivePeriod();
+    this.fetchCalendar();
+
+  }
 
   render() {
     return <div className="container">
       <Navigation next={this.nextMonth} prev={this.prevMonth} date={this.state.date}/>
-      <Table allDays={this.state.allMonthDays}/>
+      <Table allDays={this.state.allMonthDays} teams={this.state.teams} date={this.state.date}/>
     </div>
   }
 }
